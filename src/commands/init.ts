67dff5da88dd0117323ad747b18e491e5d2ed219
Siddhant Singh <siddhant06137@gmail.com>
2024-10-24 02:45:46 +0530

    Added files for init

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,26 +1,200 @@
 import { Command } from "commander"
+import { promises as fs } from "fs"
 import prompts from "prompts"
+import { z } from "zod"
+import path from "path"
+import { logger } from "@/src/utils/logger"
+import { highlighter } from "@/src/utils/highlighter"
+import { handleError } from "@/src/utils/handle-error"
+import { preFlightInit } from "@/src/preflights/preflight-init"
+import * as ERRORS from "@/src/utils/errors"
+import { createProject } from "@/src/utils/create-project"
+import { getProjectConfig, getProjectInfo } from "@/src/utils/get-project-info"
+import {
+  DEFAULT_COMPONENTS,
+  DEFAULT_TAILWIND_CONFIG,
+  DEFAULT_TAILWIND_CSS,
+  DEFAULT_UTILS,
+  getConfig,
+  rawConfigSchema,
+  resolveConfigPaths,
+  type Config,
+} from "@/src/utils/get-config"
+import { spinner } from "../utils/spinner"
+import { Ora } from "ora"
+
+export const initOptionsSchema = z.object({
+  cwd: z.string(),
+  yes: z.boolean(),
+  defaults: z.boolean(),
+  force: z.boolean(),
+  silent: z.boolean(),
+  isNewProject: z.boolean(),
+  srcDir: z.boolean().optional(),
+})
 
 export const init = new Command()
   .name("init")
-  .description("Initialize an admin-boil project with selected options")
-  .action(async () => {
-    const response = await prompts({
-      type: "select",
-      name: "projectType",
-      message: "Choose the type of project to initialize:",
-      choices: [
-        { title: "Dashboard", value: "dashboard" },
-        { title: "API Only", value: "api-only" },
-        { title: "Fullstack", value: "fullstack" },
-      ],
-    })
+  .description("initialize your project and install dependencies")
+  .option("-y, --yes", "skip confirmation prompt.", true)
+  .option("-d, --defaults,", "use default configuration.", false)
+  .option("-f, --force", "force overwrite of existing configuration.", false)
+  .option(
+    "-c, --cwd <cwd>",
+    "the working directory. defaults to the current directory.",
+    process.cwd()
+  )
+  .option("-s, --silent", "mute output.", false)
+  .option(
+    "--src-dir",
+    "use the src directory when creating a new project.",
+    false
+  )
+  .action(async (opts) => {
+    try {
+      const options = initOptionsSchema.parse({
+        cwd: path.resolve(opts.cwd),
+        isNewProject: false,
+        ...opts,
+      })
 
-    if (response.projectType) {
-      console.log(
-        `You have chosen to initialize a ${response.projectType} project.`
+      await runInit(options)
+
+      logger.log(
+        `${highlighter.success(
+          "Success!"
+        )} Project initialization completed.\nYou may now add components.`
       )
-    } else {
-      console.log("No option selected. Exiting.")
+      logger.break()
+    } catch (error) {
+      logger.break()
+      handleError(error)
     }
   })
+
+export async function runInit(
+  options: z.infer<typeof initOptionsSchema> & {
+    skipPreflight?: boolean
+  }
+) {
+  let projectInfo
+  if (!options.skipPreflight) {
+    const preflight = await preFlightInit(options)
+    if (preflight.errors[ERRORS.MISSING_DIR_OR_EMPTY_PROJECT]) {
+      const { projectPath } = await createProject(options)
+      if (!projectPath) {
+        process.exit(1)
+      }
+      options.cwd = projectPath
+      options.isNewProject = true
+    }
+    projectInfo = preflight.projectInfo
+  } else {
+    projectInfo = await getProjectInfo(options.cwd)
+  }
+
+  const projectConfig = await getProjectConfig(options.cwd, projectInfo)
+  const config = projectConfig ? projectConfig : await promptForConfig()
+
+  if (!options.yes) {
+    const { proceed } = await prompts({
+      type: "confirm",
+      name: "proceed",
+      message: `Write configuration to ${highlighter.info(
+        "components.json"
+      )}. Proceed?`,
+      initial: true,
+    })
+
+    if (!proceed) {
+      process.exit(0)
+    }
+  }
+
+  //Copy files
+  const copyFilesSpinner = spinner(`Copying files to your project.`).start()
+  await copyFiles(options, copyFilesSpinner)
+
+  //Install dependencies
+  const installDependenciesSpinner = spinner(
+    `Installing dependencies...`
+  ).start()
+  // await copyFiles(options)
+  installDependenciesSpinner.succeed()
+
+  const fullConfig = await resolveConfigPaths(options.cwd, config)
+
+  return fullConfig
+}
+
+async function promptForConfig(defaultConfig: Config | null = null) {
+  logger.info("")
+  const options = await prompts([
+    {
+      type: "toggle",
+      name: "typescript",
+      message: `Would you like to use ${highlighter.info(
+        "TypeScript"
+      )} (recommended)?`,
+      initial: defaultConfig?.tsx ?? true,
+      active: "yes",
+      inactive: "no",
+    },
+    {
+      type: "text",
+      name: "tailwindPrefix",
+      message: `Are you using a custom ${highlighter.info(
+        "tailwind prefix eg. tw-"
+      )}? (Leave blank if not)`,
+      initial: "",
+    },
+    {
+      type: "text",
+      name: "components",
+      message: `Configure the import alias for ${highlighter.info(
+        "components"
+      )}:`,
+      initial: defaultConfig?.aliases["components"] ?? DEFAULT_COMPONENTS,
+    },
+    {
+      type: "text",
+      name: "utils",
+      message: `Configure the import alias for ${highlighter.info("utils")}:`,
+      initial: defaultConfig?.aliases["utils"] ?? DEFAULT_UTILS,
+    },
+    {
+      type: "toggle",
+      name: "rsc",
+      message: `Are you using ${highlighter.info("React Server Components")}?`,
+      initial: defaultConfig?.rsc ?? true,
+      active: "yes",
+      inactive: "no",
+    },
+  ])
+
+  return rawConfigSchema.parse({
+    $schema: "https://ui.shadcn.com/schema.json",
+    rsc: options.rsc,
+    tsx: options.typescript,
+    aliases: {
+      utils: options.utils,
+      components: options.components,
+      // TODO: fix this.
+      lib: options.components.replace(/\/components$/, "lib"),
+      hooks: options.components.replace(/\/components$/, "hooks"),
+    },
+  })
+}
+
+async function copyFiles(
+  options: z.infer<typeof initOptionsSchema> & {
+    skipPreflight?: boolean
+  },
+  spinner: Ora
+) {
+  //write a delay function
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
+  //replicate a failed promise
+  spinner.fail()
+}
---
+++ src/utils/frameworks.ts
@@ -0,0 +1,69 @@
+const installationLink = "https://ui.shadcn.com/docs/installation/next"
+export const FRAMEWORKS = {
+  "next-app": {
+    name: "next-app",
+    label: "Next.js",
+    links: {
+      installation: installationLink,
+      tailwind: "https://tailwindcss.com/docs/guides/nextjs",
+    },
+  },
+  "next-pages": {
+    name: "next-pages",
+    label: "Next.js",
+    links: {
+      installation: installationLink,
+      tailwind: "https://tailwindcss.com/docs/guides/nextjs",
+    },
+  },
+  remix: {
+    name: "remix",
+    label: "Remix",
+    links: {
+      installation: installationLink,
+      tailwind: "https://tailwindcss.com/docs/guides/remix",
+    },
+  },
+  vite: {
+    name: "vite",
+    label: "Vite",
+    links: {
+      installation: installationLink,
+      tailwind: "https://tailwindcss.com/docs/guides/vite",
+    },
+  },
+  astro: {
+    name: "astro",
+    label: "Astro",
+    links: {
+      installation: installationLink,
+      tailwind: "https://tailwindcss.com/docs/guides/astro",
+    },
+  },
+  laravel: {
+    name: "laravel",
+    label: "Laravel",
+    links: {
+      installation: installationLink,
+      tailwind: "https://tailwindcss.com/docs/guides/laravel",
+    },
+  },
+  gatsby: {
+    name: "gatsby",
+    label: "Gatsby",
+    links: {
+      installation: installationLink,
+      tailwind: "https://tailwindcss.com/docs/guides/gatsby",
+    },
+  },
+  manual: {
+    name: "manual",
+    label: "Manual",
+    links: {
+      installation: installationLink,
+      tailwind: "https://tailwindcss.com/docs/installation",
+    },
+  },
+} as const
+
+export type Framework = (typeof FRAMEWORKS)[keyof typeof FRAMEWORKS]
