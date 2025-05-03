3418310d282449dcc0cf4a52b11f499179969a2f
Siddhant Singh <siddhant06137@gmail.com>
2025-05-03 16:42:15 +0530

    fixed baseURL

--- src/utils/axiosinstance.ts
+++ src/utils/axiosinstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const admin_boilInstance = axios.create({
-  baseURL: "https://admin-boil.vercel.app/api",
+  baseURL: "https://admin-boil.dev/api",
   headers: {
     Accept: "application/json",
   },
